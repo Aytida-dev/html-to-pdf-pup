b5beb1be7c36bcdbf107bd41b72e311ebdbf4d9e
Aditya raj <aytida.dev@gmail.com>
2024-05-02 22:52:36 +0530

    added limit to number of tabs and also changed the flow to event based

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const { generatePdf } = require("./lib/generatePdf")
 const { pdfEvents } = require("./lib/eventEmitter")
-const { enqueuePdf, getFirstHtmlData } = require("./lib/utils/pdfQueueUtils")
+const { enqueuePdf, getFirstHtmlData, dequeuePdf } = require("./lib/utils/pdfQueueUtils")
 
 const { htmlData } = require("./testHtml")
+const { getTab, removeTab } = require("./lib/utils/browserTabsUtils")
 
 
 pdfEvents.on("HTML_ADDED", async () => {
-    const htmlData = getFirstHtmlData()
-    await generatePdf(htmlData)
-    console.log("PDF generated successfully")
+    const tab = await getTab()
+    const html = getFirstHtmlData()
+
+    if (!tab) {
+        return
+    }
+
+    pdfEvents.emit("TAB_READY", tab, html)
 })
 
-enqueuePdf(htmlData)
+pdfEvents.on("TAB_READY", async (tab, html) => {
+    const pdfBuffer = await generatePdf(tab, html)
 
-setTimeout(() => {
-    enqueuePdf(htmlData)
+    dequeuePdf()
+    pdfEvents.emit("PDF_GENERATED", pdfBuffer)
+    await removeTab(tab)
 })
+
+const pdfGeneration = async (htmlData) => {
+    return new Promise((resolve, reject) => {
+        enqueuePdf(htmlData)
+        pdfEvents.once("PDF_GENERATED", (pdfBuffer) => {
+            resolve(pdfBuffer)
+        })
+    })
+}
+
+setInterval(() => {
+    pdfGeneration(htmlData).then((pdfBuffer) => {
+        console.log("PDF Created and returned successfully");
+    })
+}, 1000);
+
+
+
+
+
+
 
